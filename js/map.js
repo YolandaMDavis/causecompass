66007714b09b22c03cdd1f9b53aa570f40cbe321
Yolanda Davis <ydavis@advanc-ed.org>
2012-12-01 17:13:48 -0500

    'map'

---
+++ js/map.js
@@ -0,0 +1,68 @@
+
+function populateMap(charityList){
+	var charityMarkers = new Array();
+	var charity;
+	
+	$(charityList).each(function(){		
+		charity = new Array();
+		charity.push(this.name);
+		charity.push(this.lat);
+		charity.push(this.long);
+		charityMarkers.push(charity);
+	});	
+	return charityMarkers;
+}
+
+function renderGoogleMap(mapId,charityList){
+
+	var markers = populateMap(charityList);
+	
+	var myOptions = {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false
+    };
+	
+    var map = new google.maps.Map(document.getElementById("googleMap"),myOptions);
+    var infowindow = new google.maps.InfoWindow(); 
+    var marker, i;
+    var bounds = new google.maps.LatLngBounds();
+
+    for (i = 0; i < markers.length; i++) { 
+        var pos = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(pos);
+        marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+			title: markers[i][0]
+        });
+        
+		google.maps.event.addListener(marker, 'click', function(marker, i) 
+		{
+            return function() {
+                infowindow.setContent(markers[i][0]);
+                infowindow.open(map, marker);
+            }
+        });
+    }
+	
+    map.fitBounds(bounds);
+	
+}
+
+function displayCharityList(charityList){
+
+
+}
+
+function displaySearchResults(charityList){
+	renderGoogleMap('googleMap',charityList);
+	displayCharityList(charityList);
+	
+}
+
+function getDummyCharities(){
+
+	return [{name:'Salvation ',short_desc:'Giving organization',city:'Atlanta',state:'Georgia',lat:-33.890542,long:151.274856},
+	{name:'Goodwill',short_desc:'Giving organization',city:'Atlanta',state:'Georgia',lat:-33.923036,long:151.259052}];
+
+}
