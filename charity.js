987e414aa1f4108fc4127ebdfc5d4ecf8e394a54
Devraj Mehta <devraj@cosmicac.(none)>
2012-12-01 15:52:17 -0500

    First commit of charity page and js

---
+++ charity.js
@@ -0,0 +1,45 @@
+(function($){
+
+/**
+ * Loading js for single charity page.
+ * Pulls charity info from ajaxUrl
+ * Populates info into charity.php page
+ * Assumes element ids from charity.php
+ */
+CHARITY = function(id){
+	
+	var self = this;
+	
+	self.init = function(){
+		self.ajaxUrl = "/tbd-load-charity";
+		self.charityId = id;
+	};
+	
+	self.ajax = function(){
+		$.ajax(
+			url: self.ajaxUrl,
+			type: "POST",
+			data: {
+				charityId : self.charityId
+			},
+			success: function(data){
+				self.populate(data);
+			},
+			error: function(j,t,e){
+				console.log("error loading charity: "+self.charityId);
+				window.location.pathname = "/index.php";
+			}
+		);
+	};
+	
+	self.populate = function(data){
+		$('#name').html(data.name);
+		$('#desc').html(data.longDescription);
+		$('#link').html(data.link);
+	};
+	
+	
+	self.init();
+};
+
+})(jQuery);
