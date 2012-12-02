d9109c8dfd98332b1754f1d0e7cc225f8851a365
Devraj Mehta <devraj@cosmicac.(none)>
2012-12-02 12:30:11 -0500

    team pictures

--- about/index.php
+++ about/index.php
@@ -54,18 +54,81 @@
 </div>   
 <div class="container" id="charityDescription">		
 	<h1>About Us</h1>
-	
+	<p>Donors need to capture appropriate cause with an immediate impact. Potential donors get frustrated with donation sites because they lack precision and almost always post multiple overlapping input boxes with redundant information. This discourages  successful transactions. The goal is to simplify and elucidate the  process.
+	</p>
 	<div>
 	<a href="http://www.rhok.org/problems/getting-more-funding-effective-nonprofits">RHoK problem statement</a>
+	<br>
+	<a href="http://www.rhok.org/solutions/cause-compass">Our solution statement</a>
+	<br>
+	<a href="http://www.surveymonkey.com/s/8TQ8P7K">Help us out and take our survey!</a>
+	<br>
+	<a href="https://twitter.com/causecompass">Twitter</a>
+	&nbsp;	&nbsp;	&nbsp;	&nbsp;
+	<a href="https://www.facebook.com/cause.compass">Facebook</a>
 	<br>
 	<a href="https://github.com/YolandaMDavis/causecompass/">Source Code - Github Repo</a>
 	<br>
 	<a href="http://npbendre.com/wiki">Project Wiki</a>
 	<br>
 	
+	<div id="team">
+		<div class="member">
+			<img src="/images/team/1.jpg">
+			<div class="name">Scott McManus<br>GTRI</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/2.jpg">
+			<div class="name">Devraj Mehta<br>Digital Assent</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/3.jpg">
+			<div class="name">Jamil Bonnick<br>Free Lance UI Design</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/4.jpg">
+			<div class="name">Nikhil Bendre<br>Air-Watch</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/5.jpg">
+			<div class="name">GQ Lewis<br>Independent</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/6.jpg">
+			<div class="name">Luxmi Saha<br>Air-Watch</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/7.jpg">
+			<div class="name">Rose Robinson<br> Anita Borg Institute</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/8.jpg">
+			<div class="name">Rohit Bhat<br>Echostar</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/9.jpg">
+			<div class="name">Naim Hurst</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/10.jpg">
+			<div class="name">Rosario A. Robinson<br> Student</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/11.jpg">
+			<div class="name">Promita Bose<br> Emory University<br>Human Genetics</div>
+		</div>
+		<div class="member">
+			<img src="/images/team/12.jpg">
+			<div class="name">Yolanda Davis<br> AdvancED</div>
+		</div>
+	</div>
+	
 	</div>
 </div>
-
- 
+<style type="text/css">
+#team{}
+.member{ margin: 15px; float:left;position:relative;}
+.name{ position:absolute;bottom:5px;right:5px;background-color:rgba(255,255,255,0.8); border-radius:0px;padding:5px;}
+</style>
 </body>
 </html>
