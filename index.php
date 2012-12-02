a8c9b80b2537e9e896a406474dbfc8c28e6504b6
Devraj Mehta <devraj@cosmicac.(none)>
2012-12-01 22:29:39 -0500

    js is nice and wired up

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 
 <!--Javascript library api  for google-->
 <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script type="text/javascript" src="js/causes.js"></script>
 <title>Cause Compass</title>
 </head>
 <body>
@@ -52,25 +53,6 @@
   <div class="carousel-inner">
     <div class="active item" id="causeTypeDiv">
 		<div class="slide-title">Select a Cause...</div>
-		<button class="btn" id="cancer" >Cancer</button>
-		<button class="btn" id="hurricane">Hurricane Sandy</button>
-		<button class="btn" id="children">Children</button>
-		<button class="btn" id="orphanage">Orphanage</button>
-		<button class="btn" id="earthquake"> Earthquake</button>
-		<button class="btn" id="education">Education</button>
-		<button class="btn" id="hunger">Hunger & Proverty</button>
-		<button class="btn" id="flood">Flood Relief</button>
-		<button class="btn" id="veterans">Veterans</button>
-		<button class="btn" id="oldage">Old Age Homes</button>
-		<button class="btn" id="animal">Animal Relief</button>
-		<button class="btn" id="environmental">Environmental Relief</button>
-		<button class="btn" id="international">International Development NGO's</button>
-		<button class="btn" id="patient">Patient & Family Support</button>
-		<button class="btn" id="performing">Performing Arts</button>
-		<button class="btn" id="libraries">Libraries & Historical Societies</button>
-		<button class="btn" id="public">Public Broadcasting and Media</button>
-		<button class="btn" id="peace">Peace & Human Rights</button>
-		<button class="btn" id="disaster">Disaster Relief</button>
 	</div>
 	<div class="item" id="searchContainerDiv">
 	  
@@ -82,10 +64,6 @@
 	  What kind of donation are you giving?
 	  </span>
 	  </div>
-	  
-	  <button class="btn" id="monetaryBtn">Monetary</button>	  
-	  <button class="btn" id="inkindBtn">In-Kind</button>
-	  
 	</div>   
     <div class="item" id="donationTypeDiv"> 
 	  <div class="slide-title">	 
@@ -96,87 +74,7 @@
 	  Where do you want to give?
 	  </span>
 	  </div>
-	  <br>
-	  <div class="span3">
-      <form class="form-horizontal" id="locationForm">
-		  <div class="control-group">
-			<label class="control-label" for="city">City</label>
-			<div class="controls">
-				<input id="city" name="city" type="text" placeholder="City" class="input-large"/>
-			</div>
-		  </div>
-		<div class="control-group">	
-		<label class="control-label" for="inputEmail">State</label>
-			<div class="controls">
-			  <select name="stateCode" id="stateCode">
-					<option value="">Select...</option>	
-					<option value="AL">Alabama</option>
-					<option value="AK">Alaska</option>
-					<option value="AS">American Samoa</option>
-					<option value="AZ">Arizona</option>
-					<option value="AR">Arkansas</option>
-					<option value="AE">Armed Forces</option>
-					<option value="AA">Armed Forces (Americas)</option>
-					<option value="AP">Armed Forces (Pacific)</option>
-					<option value="CA">California</option>
-					<option value="CO">Colorado</option>
-					<option value="CT">Connecticut</option>
-					<option value="DE">Delaware</option>
-					<option value="DC">District of Columbia</option>
-					<option value="FL">Florida</option>
-					<option value="GA">Georgia</option>
-					<option value="GU">Guam</option>
-					<option value="HI">Hawaii</option>
-					<option value="ID">Idaho</option>
-					<option value="IL">Illinois</option>
-					<option value="IN">Indiana</option>
-					<option value="IA">Iowa</option>
-					<option value="KS">Kansas</option>
-					<option value="KY">Kentucky</option>
-					<option value="LA">Louisiana</option>
-					<option value="ME">Maine</option>
-					<option value="MD">Maryland</option>
-					<option value="MA">Massachusetts</option>
-					<option value="MI">Michigan</option>
-					<option value="MN">Minnesota</option>
-					<option value="MS">Mississippi</option>
-					<option value="MO">Missouri</option>
-					<option value="MT">Montana</option>
-					<option value="NE">Nebraska</option>
-					<option value="NV">Nevada</option>
-					<option value="NH">New Hampshire</option>
-					<option value="NJ">New Jersey</option>
-					<option value="NM">New Mexico</option>
-					<option value="NY">New York</option>
-					<option value="NC">North Carolina</option>
-					<option value="ND">North Dakota</option>
-					<option value="MP">Northern Mariana Islands</option>
-					<option value="OH">Ohio</option>
-					<option value="OK">Oklahoma</option>
-					<option value="OR">Oregon</option>
-					<option value="PA">Pennsylvania</option>
-					<option value="PR">Puerto Rico</option>
-					<option value="RI">Rhode Island</option>
-					<option value="SC">South Carolina</option>
-					<option value="SD">South Dakota</option>
-					<option value="TN">Tennessee</option>
-					<option value="TX">Texas</option>
-					<option value="UT">Utah</option>
-					<option value="VT">Vermont</option>
-					<option value="VI">Virgin Islands</option>
-					<option value="VA">Virginia</option>
-					<option value="WA">Washington</option>
-					<option value="WV">West Virginia</option>
-					<option value="WI">Wisconsin</option>
-					<option value="WY">Wyoming</option>
-				</select>				
-			</div>
-		</div>				
-	  </form>
-	  </div>
-	  <div>
-	  <button type="button" class="btn">Sign in</button>
-	  </div>
+	  
     </div>
   </div>  
 </div>
@@ -184,37 +82,6 @@
 </div>
  
 </div>
-
-<script type="text/javascript">
-$('.carousel').carousel('pause');
-
-$('#causeTypeDiv .btn').click(function(){
-	//this.id should be used to get val	
-	$('#myCarousel').carousel('next');
-});
-
-$('#donationTypeBtn').click(function(){
-	//this.id should be used to get val	
-	$('#myCarousel').carousel('next');
-});
-
-$('#monetaryBtn').click(function(){
-	//this.id should be used to get val	
-	$('#myCarousel').carousel('next');
-});
-
-$('#inkindBtn').click(function(){
-	//this.id should be used to get val	
-	$('#myCarousel').carousel('next');
-});
-
-$('.backBtn').click(function(){
-	//this.id should be used to get val	
-	$('#myCarousel').carousel('prev');
-});
-
-</script>
-
-
+<!-- Put google map and list here =) -->
 </body>
 </html>
--- js/causes.js
+++ js/causes.js
@@ -10,10 +10,15 @@ CC.charities = [];
 CC.states = [];
 CC.donation_types = [];
 
+CC.cause_id = 0;
+CC.state = "";
+CC.donation_type = 0;
+
 $.ajax({
 	url: "http://npbendre.com/causecompass/api.php?action=get_all_charities",
 	dataType: "jsonp",
 	success: function(data){
+		console.log('loaded charities'+data);
 		CC.charities = data;
 	},
 	error: function(){
@@ -25,7 +30,16 @@ $.ajax({
 	url: "http://npbendre.com/causecompass/api.php?action=get_all_cause",
 	dataType: "jsonp",
 	success: function(data){
+		console.log('loaded causes');
 		CC.causes = data;
+		$.each(CC.causes, function(index, cause){
+			console.log(cause.cause_name);
+			$('<button class="btn" id="'+cause.cause_id+'">'+cause.cause_name+'</button>').appendTo('#causeTypeDiv');
+		});
+		$('#causeTypeDiv .btn').click(function(){
+			CC.cause_id = this.id;
+			$('#myCarousel').carousel('next');
+		});
 	},
 	error: function(){
 		alert("Error loading the cause");
@@ -36,10 +50,24 @@ $.ajax({
 	url: "http://npbendre.com/causecompass/api.php?action=get_all_locations",
 	dataType: "jsonp",
 	success: function(data){
+		console.log('loaded locations');
 		CC.states = data;
+		$.each(CC.states, function(index, state){
+			console.log(state.State);
+			$('<button class="btn" id="'+state.State+'">'+state.State+'</button>').appendTo('#donationTypeDiv');
+		});
+		$('#donationTypeDiv .btn').click(function(){
+			CC.state = this.id;
+			/**
+			Yolanda, here is where you should set up your map data.
+			The relevant js data should be in CC.state, CC.donation_type, and CC.cause_id
+			This should be used here: http://npbendre.com/causecompass/api.php?action=get_charity_list&cause_id=1&state=DC&charity_type=1
+			(same ajax calls as all the others)
+			*/
+		});
 	},
 	error: function(){
-		alert("Error loading the cause");
+		alert("Error loading the locations");
 	}
 });
 
@@ -47,27 +75,30 @@ $.ajax({
 	url: "http://npbendre.com/causecompass/api.php?action=get_donation_types",
 	dataType: "jsonp",
 	success: function(data){
+		console.log('loaded donation types');
 		CC.donation_types = data;
+		$.each(CC.donation_types, function(index, type){
+			console.log(type.chaity_type_name);
+			$('<button class="btn" id="'+type.charity_type_id+'">'+type.chaity_type_name+'</button>').appendTo('#searchContainerDiv');
+		});
+		$('#searchContainerDiv .btn').click(function(){
+			CC.donation_type = this.id;
+			$('#myCarousel').carousel('next');
+		});
 	},
 	error: function(){
-		alert("Error loading the cause");
+		alert("Error loading the donation type");
 	}
 });
 
+
 $(document).ready(function(){
-	$.each(CC.causes, function(index, cause){
-		$('#slide-1').append($('<span title='+cause.cause_id+'>'+cause.cause_name+'</span>'));
-	});
+	$('.carousel').carousel('pause');
 	
-	$('#state').autocomplete({
-		source: CC.states
+	$('.backBtn').click(function(){
+		$('#myCarousel').carousel('prev');
 	});
-		
-	$('#donation').autocomplete({
-		source: CC.donation_types
-	});
-	
-	
+
 });
 
 })(jQuery);
