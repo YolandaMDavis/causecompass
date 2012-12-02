86f89196ecc6e8da1cea690ab005ca610abc7697
Yolanda Davis <ydavis@advanc-ed.org>
2012-12-01 22:06:20 -0500

    'nav'

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 
 <!--Javascript library api  for google-->
 <script type="text/javascript" src="http://www.google.com/jsapi"></script>
-<title>Google Chart API Tutorial</title>
+<title>Cause Compass</title>
 </head>
 <body>
 
@@ -73,8 +73,14 @@
 		<button class="btn" id="disaster">Disaster Relief</button>
 	</div>
 	<div class="item" id="searchContainerDiv">
-	  <div class="slide-title">
+	  
+	  <div class="slide-title">	  
+	  <span class="span1">
+	  <a class="btn backBtn" href="#"><i class="icon-arrow-left"></i></a>
+	  </span>
+	  <span class="span4">
 	  What kind of donation are you giving?
+	  </span>
 	  </div>
 	  
 	  <button class="btn" id="monetaryBtn">Monetary</button>	  
@@ -82,75 +88,95 @@
 	  
 	</div>   
     <div class="item" id="donationTypeDiv"> 
-	  <div class="slide-title">	  
+	  <div class="slide-title">	 
+	  <span class="span1">
+	  <a class="btn backBtn" href="#"><i class="icon-arrow-left"></i></a>
+	  </span>
+	  <span class="span4">
 	  Where do you want to give?
-	  </div>		
-      <form class="form-inline">
-	  
-		  <input id="city" type="text" placeholder="City" class="input-large"/>
-		  <select name="stateCode" id="stateCode">
-			<option value="">Choose...</option>	
-			<option value="AL">Alabama</option>
-            <option value="AK">Alaska</option>
-            <option value="AS">American Samoa</option>
-            <option value="AZ">Arizona</option>
-            <option value="AR">Arkansas</option>
-            <option value="AE">Armed Forces</option>
-            <option value="AA">Armed Forces (Americas)</option>
-            <option value="AP">Armed Forces (Pacific)</option>
-            <option value="CA">California</option>
-            <option value="CO">Colorado</option>
-            <option value="CT">Connecticut</option>
-            <option value="DE">Delaware</option>
-            <option value="DC">District of Columbia</option>
-            <option value="FL">Florida</option>
-            <option value="GA">Georgia</option>
-            <option value="GU">Guam</option>
-            <option value="HI">Hawaii</option>
-            <option value="ID">Idaho</option>
-            <option value="IL">Illinois</option>
-            <option value="IN">Indiana</option>
-            <option value="IA">Iowa</option>
-            <option value="KS">Kansas</option>
-            <option value="KY">Kentucky</option>
-            <option value="LA">Louisiana</option>
-            <option value="ME">Maine</option>
-            <option value="MD">Maryland</option>
-            <option value="MA">Massachusetts</option>
-            <option value="MI">Michigan</option>
-            <option value="MN">Minnesota</option>
-            <option value="MS">Mississippi</option>
-            <option value="MO">Missouri</option>
-            <option value="MT">Montana</option>
-            <option value="NE">Nebraska</option>
-            <option value="NV">Nevada</option>
-            <option value="NH">New Hampshire</option>
-            <option value="NJ">New Jersey</option>
-            <option value="NM">New Mexico</option>
-            <option value="NY">New York</option>
-            <option value="NC">North Carolina</option>
-            <option value="ND">North Dakota</option>
-            <option value="MP">Northern Mariana Islands</option>
-            <option value="OH">Ohio</option>
-            <option value="OK">Oklahoma</option>
-            <option value="OR">Oregon</option>
-            <option value="PA">Pennsylvania</option>
-            <option value="PR">Puerto Rico</option>
-            <option value="RI">Rhode Island</option>
-            <option value="SC">South Carolina</option>
-            <option value="SD">South Dakota</option>
-            <option value="TN">Tennessee</option>
-            <option value="TX">Texas</option>
-            <option value="UT">Utah</option>
-            <option value="VT">Vermont</option>
-            <option value="VI">Virgin Islands</option>
-            <option value="VA">Virginia</option>
-            <option value="WA">Washington</option>
-            <option value="WV">West Virginia</option>
-            <option value="WI">Wisconsin</option>
-            <option value="WY">Wyoming</option>
-            </select>
-		</form>
+	  </span>
+	  </div>
+	  <br>
+	  <div class="span3">
+      <form class="form-horizontal" id="locationForm">
+		  <div class="control-group">
+			<label class="control-label" for="city">City</label>
+			<div class="controls">
+				<input id="city" name="city" type="text" placeholder="City" class="input-large"/>
+			</div>
+		  </div>
+		<div class="control-group">	
+		<label class="control-label" for="inputEmail">State</label>
+			<div class="controls">
+			  <select name="stateCode" id="stateCode">
+					<option value="">Select...</option>	
+					<option value="AL">Alabama</option>
+					<option value="AK">Alaska</option>
+					<option value="AS">American Samoa</option>
+					<option value="AZ">Arizona</option>
+					<option value="AR">Arkansas</option>
+					<option value="AE">Armed Forces</option>
+					<option value="AA">Armed Forces (Americas)</option>
+					<option value="AP">Armed Forces (Pacific)</option>
+					<option value="CA">California</option>
+					<option value="CO">Colorado</option>
+					<option value="CT">Connecticut</option>
+					<option value="DE">Delaware</option>
+					<option value="DC">District of Columbia</option>
+					<option value="FL">Florida</option>
+					<option value="GA">Georgia</option>
+					<option value="GU">Guam</option>
+					<option value="HI">Hawaii</option>
+					<option value="ID">Idaho</option>
+					<option value="IL">Illinois</option>
+					<option value="IN">Indiana</option>
+					<option value="IA">Iowa</option>
+					<option value="KS">Kansas</option>
+					<option value="KY">Kentucky</option>
+					<option value="LA">Louisiana</option>
+					<option value="ME">Maine</option>
+					<option value="MD">Maryland</option>
+					<option value="MA">Massachusetts</option>
+					<option value="MI">Michigan</option>
+					<option value="MN">Minnesota</option>
+					<option value="MS">Mississippi</option>
+					<option value="MO">Missouri</option>
+					<option value="MT">Montana</option>
+					<option value="NE">Nebraska</option>
+					<option value="NV">Nevada</option>
+					<option value="NH">New Hampshire</option>
+					<option value="NJ">New Jersey</option>
+					<option value="NM">New Mexico</option>
+					<option value="NY">New York</option>
+					<option value="NC">North Carolina</option>
+					<option value="ND">North Dakota</option>
+					<option value="MP">Northern Mariana Islands</option>
+					<option value="OH">Ohio</option>
+					<option value="OK">Oklahoma</option>
+					<option value="OR">Oregon</option>
+					<option value="PA">Pennsylvania</option>
+					<option value="PR">Puerto Rico</option>
+					<option value="RI">Rhode Island</option>
+					<option value="SC">South Carolina</option>
+					<option value="SD">South Dakota</option>
+					<option value="TN">Tennessee</option>
+					<option value="TX">Texas</option>
+					<option value="UT">Utah</option>
+					<option value="VT">Vermont</option>
+					<option value="VI">Virgin Islands</option>
+					<option value="VA">Virginia</option>
+					<option value="WA">Washington</option>
+					<option value="WV">West Virginia</option>
+					<option value="WI">Wisconsin</option>
+					<option value="WY">Wyoming</option>
+				</select>				
+			</div>
+		</div>				
+	  </form>
+	  </div>
+	  <div>
+	  <button type="button" class="btn">Sign in</button>
+	  </div>
     </div>
   </div>  
 </div>
@@ -180,6 +206,11 @@ $('#monetaryBtn').click(function(){
 $('#inkindBtn').click(function(){
 	//this.id should be used to get val	
 	$('#myCarousel').carousel('next');
+});
+
+$('.backBtn').click(function(){
+	//this.id should be used to get val	
+	$('#myCarousel').carousel('prev');
 });
 
 </script>
